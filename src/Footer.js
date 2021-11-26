f7470b99ff028f764408df73e5ce833e36684220
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-11-26 02:49:00 +0000

    Initialize

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <p>eemeelyo 2021</p>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
+        <h1>React Calculator</h1>
+
+        <table className="table">
+          <tr>
+            <td colSpan="3">
+              <input type="text" />
+            </td>
+            <td>
+              <button>Clear</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button>1</button>
+            </td>
+            <td>
+              <button>2</button>
+            </td>
+            <td>
+              <button>3</button>
+            </td>
+            <td>
+              <button>/</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button>4</button>
+            </td>
+            <td>
+              <button>5</button>
+            </td>
+            <td>
+              <button>6</button>
+            </td>
+            <td>
+              <button>-</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button>7</button>
+            </td>
+            <td>
+              <button>8</button>
+            </td>
+            <td>
+              <button>9</button>
+            </td>
+            <td>
+              <button>+</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button>.</button>
+            </td>
+            <td>
+              <button>0</button>
+            </td>
+            <td>
+              <button>=</button>
+            </td>
+            <td>
+              <button>*</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+export default Main;
